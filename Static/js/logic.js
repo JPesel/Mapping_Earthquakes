5877ad42a1584049240e75f4bfea7c72cee01986
JPesel <jacobo@pesel.org>
2021-10-17 23:10:40 -0500

    adding toglleable earthquakes layer

--- Static/js/logic.js
+++ Static/js/logic.js
@@ -49,21 +49,30 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-// Create a base layer that holds both maps.
-let baseMaps = {
-    "Street": streets,
-    "Satellite": satelliteStreets
-};
-
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
     center: [39.5, -98.5],
     zoom: 3,
     layers: [streets]
 })
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Street": streets,
+    "Satellite": satelliteStreets
+};
 
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+    Earthquakes: earthquakes
+};
+
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Accessing the toronto GeoJSON URL
 let torontoHoods = "https://raw.githubusercontent.com/JPesel/Mapping_Earthquakes/main/torontoNeighborhoods.json";
@@ -84,12 +93,32 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         return {
         opacity: 1,
         fillOpacity: 1,
-        fillColor: "#ffae42",
+        fillColor: getColor(feature.properties.mag),
         color: "#000000",
         radius: getRadius(feature.properties.mag),
         stroke: true,
         weight: 0.5
         };
+    }
+
+    // This function determines the color of the circle based on the magnitude of the earthquake.
+    function getColor(magnitude) {
+        if (magnitude > 5) {
+        return "#ea2c2c";
+        }
+        if (magnitude > 4) {
+        return "#ea822c";
+        }
+        if (magnitude > 3) {
+        return "#ee9c00";
+        }
+        if (magnitude > 2) {
+        return "#eecc00";
+        }
+        if (magnitude > 1) {
+        return "#d4ee00";
+        }
+        return "#98ee00";
     }
 
     // This function determines the radius of the earthquake marker based on its magnitude.
@@ -109,8 +138,12 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
                 console.log(data);
                 return L.circleMarker(latlng);
             },
-        style: styleInfo
-    }).addTo(map);
+        style: styleInfo,
+    
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+          }
+    }).addTo(earthquakes);
 });
 
 // // Accessing the airport GeoJSON URL
