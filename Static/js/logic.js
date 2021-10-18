c0330ae45a1abcf0fc52db336b5eaa570177ac78
JPesel <jacobo@pesel.org>
2021-10-17 21:42:24 -0500

    Adding airports json

--- Static/js/logic.js
+++ Static/js/logic.js
@@ -6,9 +6,30 @@ let line = [
     [37.6213, -122.3790],
     [40.7899, -111.9791],
     [47.4502, -122.3088]
-  ];
+];
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([30, 30], 2);
 
 
 // We create the tile layer that will be the background of our map.
@@ -24,18 +45,31 @@ streets.addTo(map);
 // Get data from cities.js
 let cityData = cities;
 
-// Create a polyline using the line coordinates and make the line black.
-L.polyline(line, {
-    color: "yellow"
- }).addTo(map);
+// // Create a polyline using the line coordinates and make the line black.
+// L.polyline(line, {
+//     color: "yellow"
+// }).addTo(map);
 
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location,{
-        radius:city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location,{
+//         radius:city.population/100000
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport).addTo(map);
+
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(feature);
+      return L.marker(latlng).bindPopup("<h2>" + feature.properties.city + "</h2>");
+    }
+
+  }).addTo(map);
